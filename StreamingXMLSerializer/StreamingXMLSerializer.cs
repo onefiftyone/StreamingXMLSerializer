19539a48a36598c1a050983f92216873297e3fba
onefiftyone <shelby@shelbyrobertson.com>
2015-02-04 22:28:02 -0500

    Added Generic Serializer class and basic functionality.

---
+++ StreamingXMLSerializer/StreamingXMLSerializer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace OneFiftyOne.Serialization.StreamingXMLSerializer
+{
+    public class StreamingXMLSerializer
+    {
+        #region PRIVATES
+        private static XmlWriterSettings xmlWriterSettings = new XmlWriterSettings()
+        {
+            Indent = true,
+            IndentChars = "\t",
+            OmitXmlDeclaration = true
+        };
+        #endregion
+
+        #region SERIALIZE
+
+        public static void Serialize(object data, string outputFile)
+        {
+            using (var writer = XmlWriter.Create(outputFile, xmlWriterSettings))
+            {
+                serializeInternal(data, writer);
+            }
+        }
+
+        private static void serializeInternal(object data, XmlWriter writer, string nodeName = "")
+        {
+            if (data is IEnumerable && !(data is string))
+            {
+                nodeName = string.IsNullOrEmpty(nodeName) ? "List" : nodeName;
+                serializeEnumberable(data as IEnumerable, writer, nodeName);
+            }
+            else
+            {
+                nodeName = string.IsNullOrEmpty(nodeName) ? data.GetType().Name : nodeName;
+                serializeObject(data, writer, nodeName);
+            }
+        }
+
+        private static void serializeEnumberable(IEnumerable data, XmlWriter writer, string nodeName)
+        {
+            if (data == null)
+                return;
+
+            if (!string.IsNullOrWhiteSpace(nodeName))
+                writer.WriteStartElement(nodeName);
+
+            foreach(var datum in data)
+                serializeInternal(datum, writer, datum.GetType().Name);
+
+            if (!string.IsNullOrWhiteSpace(nodeName))
+                writer.WriteEndElement();
+        }
+
+        private static void serializeObject(object data, XmlWriter writer, string nodeName)
+        {
+            if (data == null)
+                return;
+
+            //if primitive type or string, output the value and return
+            var dataType = data.GetType();
+            if (dataType.IsPrimitive || data is string)
+            {
+                //primitive must have a node name
+                nodeName = string.IsNullOrEmpty(nodeName) ? "Item" : nodeName;
+                writer.WriteElementString(nodeName, data.ToString());
+                return;
+            }
+
+            if (!string.IsNullOrWhiteSpace(nodeName))
+                writer.WriteStartElement(nodeName);
+
+            var props = dataType.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+
+            foreach (var prop in props)
+                serializeInternal(prop.GetValue(data, null), writer, prop.Name);
+           
+            if (!string.IsNullOrWhiteSpace(nodeName))
+                writer.WriteEndElement();
+        }
+
+        #endregion
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -8,29 +8,34 @@ using System.Data;
 
 namespace Test
 {
+    class TestA
+    {
+        public string Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public int Prop3 { get; set; }
+
+        public TestB ClassB { get; set; }
+    }
+
+    class TestB
+    {
+        public double Prop4 { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            //using (var dt = new StreamingDataTable("ROADWAY"))
-            //{
-            //    dt.ReadXML("../../test-file-output-dt.xml");
+            var t = new TestA { Prop1 = "bacon", Prop2 = "Tomato", Prop3 = 42 };
+            var t2 = new TestA {
+                Prop1 = "lettuce", Prop2 = "Olive", Prop3 = 421,
+                ClassB = new TestB { Prop4 = 3.14 }
+            };
 
-            //    using (var dtout = new StreamingDataTable("ROADWAY"))
-            //    {
-            //        dtout.DataSource = dt;
-            //        dtout.WriteXML("../../test-file-output-streaming-dt.xml");
-            //    }
-            //}
-
-            StreamingDataTable.TransformXML("../../test-file-output-dt.xml", "../../test-file-output-streaming-dt.xml", transformFunc);
-        }
-
-        public static StreamingDataRow transformFunc(StreamingDataRow dr)
-        {
-            if (dr["BACON"] == null)
-                dr["BACON"] = "Not null";
-            return dr;
+            var list = new List<TestA>();
+            list.Add(t);
+            list.Add(t2);
+            StreamingXMLSerializer.Serialize(list, "../../TestA.xml");
         }
     }
 }
